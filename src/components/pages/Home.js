528c612ca68cd5749d18b46ffb8b1be696c2b053
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-02 14:10:57 -0300

    Created Footer component and FooterStyles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import '../../App.css';
 import Cards from '../Cards/Cards';
 import Hero from '../HeroSection/Hero';
+import Footer from '../Footer/Footer'
 
 function Home() {
     return (
         <>
             <Hero />
             <Cards />
+            <Footer />
         </>
     )
 }
