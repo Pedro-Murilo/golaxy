669625d4547fa9df64fda598260484107ab167a3
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-02-02 13:36:44 -0300

    Created Card component and added styles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import '../../App.css';
+import Cards from '../Cards/Cards';
 import Hero from '../HeroSection/Hero';
 
 function Home() {
     return (
         <>
             <Hero />
+            <Cards />
         </>
     )
 }
